b10309f9e56944d49445e581c3551ead1e353e40
mohsenbahrami <mohsen.bahrami60@gmail.com>
2021-09-27 23:02:35 -0400

    objects

---
+++ src/main/java/com/sg/foundations/basics/Objects/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.basics.Objects;
+
+/**
+ *
+ * @author bafsharim
+ */
+public class HelloWorld {
+    public void sayHello() {
+        System.out.println("Hello, World!");
+    }
+}
